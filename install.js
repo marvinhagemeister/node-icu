a7fff2d57aeb84efdf991b2ab50f117b232957d2
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-05-16 08:39:46 +0200

    Properly extract tar file

--- install.js
+++ install.js
@@ -1,6 +1,8 @@
 const http = require("https");
 const path = require("path");
 const fs = require("fs");
+const tar = require("tar");
+const rimraf = require("rimraf");
 
 const icu = process.versions.icu !== undefined
   ? process.versions.icu
@@ -31,8 +33,7 @@ const minor = {
 const icumin = icuend;
 const pkg = `icu4c-data-0.${icumaj}.${minor[icuend]}`;
 const url = `https://registry.npmjs.org/icu4c-data/-/${pkg}.tgz`;
-
-const dest = path.resolve(__dirname, `icudt${icumaj}${icuend}.dat`);
+const filename = `icudt${icumaj}${icuend}`;
 
 function download(url, dest) {
   const file = fs.createWriteStream(dest);
@@ -50,6 +51,33 @@ function download(url, dest) {
   });
 }
 
-download(url, dest).catch(err => {
+function extract(file) {
+  const outputFile = path.join(__dirname, filename + ".dat");
+  const output = fs.createWriteStream(outputFile);
+
+  fs.createReadStream(file).pipe(tar.t()).on("entry", entry => {
+    if (/\.dat$/.test(entry.path)) {
+      entry.pipe(output);
+    }
+  });
+}
+
+function install() {
+  const tmp = path.join(__dirname, "tmp");
+  if (!fs.existsSync(tmp)) {
+    fs.mkdirSync(tmp);
+  }
+
+  const dest = path.join(tmp, filename + ".tgz");
+  return download(url, dest)
+    .then(() => extract(dest))
+    .then(() => rimraf.sync(tmp))
+    .catch(err => {
+      rimraf.sync(tmp);
+      throw err;
+    });
+}
+
+install().catch(err => {
   throw err;
 });
